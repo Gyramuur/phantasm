a61bd22bc00697c260ceb76a1c7e6b357631da0b
Gyramuur <thecrux42@gmail.com>
2018-06-19 16:20:28 -0400

    Don't mind me, just not using kivy over here.

--- game.py
+++ game.py
@@ -12,6 +12,9 @@ root = Tk()
 root.configure(background="#e2c69d")
 root.title("Phantasm")
 
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
 game_text = StringVar()
 player_choice = StringVar()
 
@@ -21,11 +24,12 @@ gui_style.configure('My.TFrame', background='#e2c69d', foreground='#FFFFFF')
 content = ttk.Frame(root, style='My.TFrame', padding='5 5 5 5')
 content2 = ttk.Frame(root, style='My.TFrame', padding='5 5 5 5')
 
+game_font = ("Legendaria", 24, 'bold')
 
 entry = Entry(content2, textvariable=player_choice, background='#e2c69d', foreground='#7e2713', borderwidth=0,
-              highlightthickness=0, insertbackground='#7e2713', font=("Legendaria", 24, 'bold'))
-entry_label = Label(content2, text="> ", background='#e2c69d', foreground='#7e2713', font=("Legendaria", 24, 'bold'))
-text_field = Text(content, background='#e2c69d', foreground="#7e2713", borderwidth=0, font=("Legendaria", 24, 'bold'),
+              highlightthickness=0, insertbackground='#7e2713', font=game_font)
+entry_label = Label(content2, text="> ", background='#e2c69d', foreground='#7e2713', font=game_font)
+text_field = Text(content, background='#e2c69d', foreground="#7e2713", borderwidth=0, font=game_font,
                   wrap=WORD)
 
 
@@ -36,7 +40,7 @@ entry.grid(column=1, row=2, sticky=W, columnspan=2)
 text_field.grid(columnspan=2, row=1, sticky=(N, W, E, S))'''
 
 content.grid(sticky=(N, W), column=0, row=0)
-content2.grid(sticky=(S, W), column=0, row=1)
+content2.grid(sticky=(S, W), column=0, row=0)
 text_field.grid(column=0, sticky=(N, W))
 entry_label.grid(column=0, row=0, sticky=(S, W))
 entry.grid(column=1, row=0, sticky=(S, W))
@@ -47,6 +51,10 @@ content2.columnconfigure(0, weight=1)
 content2.rowconfigure(1, weight=1)
 text_field.columnconfigure(0, weight=1)
 text_field.rowconfigure(0, weight=1)
+
+if root.winfo_width() > screen_width and root.winfo_height() > screen_height:
+    root.geometry(f"{screen_width}x{screen_height}")
+
 root.resizable(width=False, height=False)
 
 
